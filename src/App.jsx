556217ae831dc68354890c67eb87fafacd1709fd
Daniel Garibay <danielgaribay366@gmail.com>
2025-03-17 13:19:02 -0500

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import './App.css'
 import {puppyList} from './data.js'
+import './App.css'
+
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
   console.log(puppies);
   const featuredPup = puppies.find((pup)=> pup.id === featPupId)
-  console.log(featuredPup);
+  console.log("featuredPup: ", featuredPup);
   
   return (
     <>
