0224bc93be3c36f55af789e9593c4c36d3846d2d
Daniel Garibay <danielgaribay366@gmail.com>
2025-03-17 09:39:14 -0500

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,47 @@
 import { useState } from 'react'
-
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log(puppies);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
+  
   return (
     <>
-      
-      {/* <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      {featPupId && (
+        <div className="pupInfo">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+     return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+   })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */ }
     </>
   )
 }
-
 export default App
+
+//    })
+// }
+//       {/* <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+        
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p> */ }
+//     </>
+//   )
+// }
